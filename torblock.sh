5f17b83beacb7808822f63146026ab136df3b221
Aaron Green <13464942+greenbambooaaron@users.noreply.github.com>
2020-08-25 18:45:27 +0900

    Update torblock.sh

--- torblock.sh
+++ torblock.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo -e "Getting Tor node list from github mirrored from dan.me.uk"
-wget -q -O - https://raw.githubusercontent.com/greenbambooaaron/random/master/torlist20200823 > /tmp/full.tor
+echo -e "Getting Tor exit node list from GITHUB (mirrored from dan.me.uk)"
+wget -q -O - https://raw.githubusercontent.com/greenbambooaaron/random/master/torexitlist20200825 > /tmp/full.tor
 CMD=$(cat /tmp/full.tor | uniq | sort)
 for IP in $CMD; do
     let COUNT=COUNT+1
     ufw deny from $IP
 done
-echo -e "UFW now blocking TOR connections"
+echo -e "UFW now blocking TOR Exit Nodes"
